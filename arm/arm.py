508755399721cb2c1994545ee7f91408f077ec34
jxsn <non>
2020-10-17 00:47:00 +0200

    implementation of invers kinemetics via gradient descent and visualization for 3dof

--- arm/arm.py
+++ arm/arm.py
@@ -1,18 +1,65 @@
 from servo import Servo
-import RPi.GPIO as GPIO
+import numpy as np
+import math
 
 #init the arm
 #alpha = 13, beta = 6, gamma = 22, delta = 5
-servos = [Servo(13), Servo(6), Servo(22), Servo(5)]
+servos = [Servo(13), Servo(6), Servo(22)]
+x, y, z = 0, 0, 0
 
 def close():
     for servo in servos:
         servo.close()
     GPIO.cleanup()
 
-def move_arm(*angles):
+def move(*angles):
     for angle, servo in zip(angles, servos):
         servo.move(angle)
 
-def get_angles():
-    return [servo.angle for servo in servos]
+def set_position(*position):
+    x, y, z = position
+
+def get_position():
+    return x, y, z
+
+def _current_position():
+    angles = get_current_angles()
+    return np.cos(angles).sum(), np.sin(angles).sum()
+
+def move_to_position(position=None):
+    if position is None:
+        position = get_position()
+    #TODO: make the invers kinemetics
+    position = np.array(position)
+    current_angles = get_current_angles()
+    
+    #gradient descent to move the arm
+    target = np.array([math.sqrt(position[0]**2 + position[1]**2), position[2]])
+    error = target - [np.cos(current_angles[:2]).sum(), np.sin(current_angles[:2]).sum()]
+
+    if np.linalg.norm(error) == 0:
+        return
+
+    gamma = np.arctan2(error[1], error[0])
+    target = target - error / np.linalg.norm(error)
+
+    pos2 = [np.cos(current_angles[0]).sum(), np.sin(current_angles[0]).sum()]
+    error = target - pos2
+
+    if np.linalg.norm(error) == 0:
+        return
+
+    beta = np.arctan2(error[1], error[0])
+    target = target - error / np.linalg.norm(error)
+
+    error = target
+
+    if np.linalg.norm(error) == 0:
+        return
+
+    alpha = np.arctan2(error[1], error[0])
+    
+    move(alpha, beta, gamma)
+
+def get_current_angles():
+    return np.array([servo.angle for servo in servos])
--- arm/servo.py
+++ arm/servo.py
@@ -1,24 +1,23 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import math
 
 class Servo:
 
-    def __init__(self, pin, default_delay=0):
+    def __init__(self, pin):
         self.pin = pin
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.OUT)
-        self.servo = GPIO.PWM(pin, 50)
-        self.servo.start(1)
-        self.default_delay = default_delay
-        self.angle = 0
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(pin, GPIO.OUT)
+        #self.servo = GPIO.PWM(pin, 50)
+        #self.servo.start(1)
+        self.angle = 0.0
 
     def move(self, angle):
-        angle = math.degrees(angle)
         self.angle = angle
+        angle = math.degrees(angle)
         angle = angle / 18 + 2
-        self.servo.ChangeDutyCycle(angle)
-        time.sleep(self.default_delay / 1000)
+        #self.servo.ChangeDutyCycle(angle)
 
     def close(self):
-        self.servo.stop()
+        #self.servo.stop()
+        print("close!")
