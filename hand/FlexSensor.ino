a14038b6b2693217892cc3feb9029a5c3092df62
18Markus1984 <52409213+18Markus1984@users.noreply.github.com>
2020-11-28 22:11:06 +0100

    Wrong Digital Pins

--- hand/FlexSensor.ino
+++ hand/FlexSensor.ino
@@ -1,5 +1,5 @@
 const int flexPin = A0; //The AnalogPin that reads the different values 
-const int selectedPin[3] = {D1, D2, D3}; //The pins for the InputState (maybe not the right pins)
+const int selectedPin[3] = {D6, D5, D0}; //The pins for the InputState (maybe not the right pins)
 int flexValue[5];    //Container for all the flexValues
 
 void setup() {
@@ -22,9 +22,9 @@ void loop() {
          digitialWrite(selectedPin[i], (pin >> i & 1) ? HIGH : LOW);    //Write the Values 1 or 0 --> Verunden von bzw 1(001) & S1(001) = 1 || 1(001) & S2(010) = 0
       }
       flexValue[pin] = analogRead(flexPin)   //read the Value from the selected pin
-      flexSensor = map(flexSensor,0,1023,0,100); //calculate the procentage
+      //flexSensor = map(flexSensor,0,1023,0,100); //calculate the procentage
       Serial.print(flexValue[pin] + "\t")    //print the value
    }
    Serial.println();
-   delay(1000); //1 secound brake for the esp
+   delay(100); //1 secound brake for the esp
 }
